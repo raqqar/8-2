d1137761a9b44ef3c5393f13d80ac1a506c41e7d
Viacheslav Alekseev <VA_Alekseev@outlook.com>
2022-03-23 19:59:18 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,52 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskDialog = Autodesk.Revit.UI.TaskDialog;
+
+namespace ExportNWC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            NavisworksExportOptions nweOptions = new NavisworksExportOptions();
+            
+            string path = "";
+
+
+            
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                path = dialog.FileName;
+            }
+            else
+            {
+                return Result.Succeeded;
+            }
+
+            using (var ts = new Transaction(doc, "export"))
+            {
+                ts.Start();
+                doc.Export(path, "ExportNWC", nweOptions);
+                ts.Commit();
+            }
+            TaskDialog.Show("Cообщение", "Экспортирование в формат NWC завершено");
+            return Result.Succeeded;
+        }
+    }
+}
